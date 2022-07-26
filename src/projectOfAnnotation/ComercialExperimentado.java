994fa93e11f1093d6d66d798b903c84b019454b8
JhanAntezana <jhan.antezana.y@uni.pe>
2022-07-25 22:23:59 -0500

    implementación fallida de PostConstruct y PreDestroy

--- src/projectOfAnnotation/ComercialExperimentado.java
+++ src/projectOfAnnotation/ComercialExperimentado.java
@@ -6,8 +6,18 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("comercialExp")
-@Scope("prototype")
+//@Scope("prototype")
 public class ComercialExperimentado implements Empleado{
+    //ejecución de código después de la creación del Bean
+    //@PostConstruct
+    public void ejecutaDespuesCreacion(){
+        System.out.println("Ejecutado tras la creación del Bean");
+    }
+    //ejecución de código despues de apagado del contenedor de Spring
+    //@PreDestroy
+    public void ejecutaAntesDestrucción(){
+        System.out.println("Ejecutando antes de la destrucción");
+    }
     @Autowired
     @Qualifier("informeFinancieroAnual")
     private CrearInformeFinanciero informeFinanciero;
